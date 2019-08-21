82274e2a2a50b221ba7619de9b625f01d861866e
Anton Perekhozhuk <pentatonix1997@gmail.com>
2019-08-21 20:19:35 +0300

    Adding done!

--- ex08-blockchain/blockchain.go
+++ ex08-blockchain/blockchain.go
@@ -6,16 +6,9 @@ import (
 	"bytes"
 	"strconv"
 	"crypto/sha256"
-	"math/big"
-	"math"
     "os"
 	"database/sql"
     _ "github.com/mattn/go-sqlite3"
-)
-
-const (
-	targetBits = 24
-	maxNonce = math.MaxInt64
 )
 
 type Block struct {
@@ -23,97 +16,55 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
-	Nonce         int
 }
 
-func IntToHex(n int64) []byte {
-    return []byte(strconv.FormatInt(n, 16))
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
-	pow := NewProofOfWork(block)
-	nonce, hash := pow.Run()
-	block.Hash = hash[:]
-	block.Nonce = nonce
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
 	return block
-}
-
-type Blockchain struct {
-	blocks []*Block
-}
-
-func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
 }
 
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
 }
 
-func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
-}
-
-type ProofOfWork struct {
-	block  *Block
-	target *big.Int
-}
-
-func NewProofOfWork(b *Block) *ProofOfWork {
-	target := big.NewInt(1)
-	target.Lsh(target, uint(256-targetBits))
-	pow := &ProofOfWork{b, target}
-	return pow
-}
-
-func (pow *ProofOfWork) prepareData(nonce int) []byte {
-	data := bytes.Join(
-		[][]byte{
-			pow.block.PrevBlockHash,
-			pow.block.Data,
-			IntToHex(pow.block.Timestamp),
-			IntToHex(int64(targetBits)),
-			IntToHex(int64(nonce)),
-		},
-		[]byte{},
-	)
-	return data
-}
-
-func (pow *ProofOfWork) Run() (int, []byte) {
-	var hashInt big.Int
-	var hash [32]byte
-	nonce := 0
-	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
-	for nonce < maxNonce {
-		data := pow.prepareData(nonce)
-		hash = sha256.Sum256(data)
-		//fmt.Printf("\r%x\n", hash)
-		hashInt.SetBytes(hash[:])
-		if hashInt.Cmp(pow.target) == -1 {
-			break
-		} else {
-			nonce++
-		}
-	}
-	fmt.Print("\n\n")
-	return nonce, hash[:]
-}
-
-func (pow *ProofOfWork) Validate() bool {
-	var hashInt big.Int
-	data := pow.prepareData(pow.block.Nonce)
-	hash := sha256.Sum256(data)
-	hashInt.SetBytes(hash[:])
-	isValid := hashInt.Cmp(pow.target) == -1
-	return isValid
+func AppendBlock(db *sql.DB, b *Block) {
+	db.Exec("INSERT INTO blockchain (timestamp,data,hash,prevHash)	values ($1,$2,$3,$4)",
+		b.Timestamp, b.Data, b.Hash, b.PrevBlockHash)
 }
 
 func CreateTable(db *sql.DB) {
-	db.Exec("CREATE TABLE blockcain id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp INTEGER, data TEXT, hash TEXT, prevHash TEXT")
+	db.Exec("CREATE TABLE IF NOT EXISTS blockchain (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp INTEGER, data TEXT, hash TEXT,prevHash TEXT)")
+	row :=db.QueryRow("SELECT COUNT(*) FROM blockchain")
+    count := 0
+    row.Scan(&count)
+    if count == 0 {
+		AppendBlock(db, NewGenesisBlock())
+	}
+}
+
+func getPrevHash(db *sql.DB) []byte {
+    row := db.QueryRow("SELECT hash FROM blockchain WHERE id=(SELECT MAX(id) FROM blockchain)")
+    var prevHash []byte
+	row.Scan(&prevHash) 
+	return prevHash
+}
+
+func AddBlock(db *sql.DB, data string) {
+    prevHash := getPrevHash(db)
+    b := NewBlock(data, prevHash)
+    b.PrevBlockHash = prevHash
+	AppendBlock(db, b)
+}
+
+func PrintBlockChain(db *sql.DB) {
 	
 }
 
@@ -124,16 +75,16 @@ func main() {
         panic(err)
     }
     defer db.Close()
-    bc := NewBlockchain()
-	CreateTable(db)
+    CreateTable(db)
+
 	if os.Args[1] == "add" {
         fmt.Print("Please, take data : ")
 		fmt.Scanf("%s", &data)
-		//bc.AddBlock(data)
+		AddBlock(db, data)
 		
 	}
 	if os.Args[1] == "list" {
-		fmt.Println("Printing blockchain")
+		PrintBlockChain(db)
 	}
 }
 

7699c377267766ef9be19e2a1630670e9538ed71
Anton Perekhozhuk <pentatonix1997@gmail.com>
2019-08-22 18:39:42 +0300

    write structure

--- ex11-orderbook/orderbook.go
+++ ex11-orderbook/orderbook.go
@@ -1,15 +1,56 @@
-package orderbook
+//package orderbook
+package main
+
+type Trade struct {
+	Bid    *Order
+	Ask    *Order
+	Volume uint64
+	Price  uint64
+}
 
 type Orderbook struct {
-	// 
+	Bids []MyOrder
+	Asks []MyOrder
+}
+
+type MyOrder struct {
+	Id    int
+	Kind  byte     // 2 - LIMIT, 1 - MARKET
+	Count uint64
+	Price uint64
+}
+
+type Order struct {
+	Id     int
+	Side   byte
+	Kind   byte
+	Count  uint64
+	Price  uint64
 }
 
 func New() *Orderbook {
-	// 
-	return &Orderbook{}
+	Bids := make([]MyOrder,0)
+	Asks := make([]MyOrder,0)
+	Orderbook := Orderbook{Bids,Asks}
+	return &Orderbook
+}
+
+func handle_bid(orderbook *Orderbook, order *MyOrder) ([]*Trade, *Order) {
+	return nil, nil
+}
+
+func handle_ask(orderbook *Orderbook, order *MyOrder) ([]*Trade, *Order) {
+	return nil, nil
 }
 
 func (orderbook *Orderbook) Match(order *Order) ([]*Trade, *Order) {
-	// 
-	return nil, nil
+	MyOrder := MyOrder{order.Id, order.Kind, order.Count, order.Price}
+	if order.Side == 1 {
+		return handle_bid(orderbook, &MyOrder)
+	}
+	return handle_ask(orderbook, &MyOrder)
+}
+
+func main() {
+
 }
